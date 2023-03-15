749d379cee683dadf57c2e5bcb318effa96a5844
CSapprenticeWT <38578892+CSapprenticeWT@users.noreply.github.com>
2023-03-15 23:42:22 +0800

    <<

--- java_tutorial/java8OnAction/src/main/java/com/swtao/Main.java
+++ java_tutorial/java8OnAction/src/main/java/com/swtao/Main.java
@@ -2,6 +2,9 @@ package com.swtao;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int i = 2;
+        System.out.println(i << 1);
+
     }
 }
